bbc1db1e0c2358e489755699907b1f62de3654af
unknown <david72y@gmail.com>
2019-03-01 14:26:30 -0500

    primer commit

---
+++ includes/pie.php
@@ -0,0 +1,11 @@
+
+			<div class="clearfix"></div>
+		</div> <!-- fin contenedor -->
+
+		<!-- PIE DE PÁGINA -->
+		<footer id="pie">
+			<p>Desarrollado por David EPC &copy; 2019</p>
+		</footer>
+		
+	</body>
+</html>
